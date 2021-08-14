7521477d3d968262fe2dcb09678207006ae76656
jcanaviri <jcanaviri20@gmail.com>
2021-08-13 21:06:44 -0400

    Propiedades de una clase

--- poo.php
+++ poo.php
@@ -14,3 +14,19 @@ class ClaseHija extends ClaseBase {}
 // Si no queremos que una clase pueda ser heredada
 // usamos el keyword final
 final class NombreClaseFinal {}
+
+
+// Instanciar una clase
+class Persona {
+    public $nombre;
+    public $edad;
+    private $genero;
+    protected $pasaporte;
+}
+
+$persona = new Persona();
+
+class PersonaTipoSangrePositivo extends Persona {
+    // Esta propiedad no cambiara
+    public static $tipo_sangre = 'A+';
+}
