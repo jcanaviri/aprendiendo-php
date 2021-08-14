e895766feb0b03046983ca67d2cc721011baca0e
jcanaviri <jcanaviri20@gmail.com>
2021-08-13 20:57:32 -0400

    Creacion de clases

---
+++ poo.php
@@ -0,0 +1,16 @@
+<?php
+
+// Formas de definir una clase
+class NombreClase {}
+
+abstract class NombreClaseAbstracta {}
+
+
+// Herencia de Clases
+class ClaseBase {}
+
+class ClaseHija extends ClaseBase {}
+
+// Si no queremos que una clase pueda ser heredada
+// usamos el keyword final
+final class NombreClaseFinal {}
